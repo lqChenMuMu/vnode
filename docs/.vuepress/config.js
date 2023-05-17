4c5b68c2b5167bf7b7c7c8ce52fe75190039fc93
chenlin <1243531712@qq.com>
2023-05-17 23:13:57 +0800

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+import { defaultTheme } from 'vuepress'
+
+export default {
+  title: 'DouBian',
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+	sidebar: [
+      // SidebarItem
+      {
+        text: '文档',
+        link: '/guide/',
+        children: [
+          // SidebarItem
+          {
+            text: 'git',
+            link: '/guide/git.md',
+            children: [],
+          },
+        ],
+      },
+	 ]
+  }),
+}
